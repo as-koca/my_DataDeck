49e4ac6ed709e2ebb27fdb8bbd5795c87913a595
akoca <akoca@f6r6s11.paris.42.school>
2026-06-01 17:04:15 +0200

    ...

---
+++ battle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from ex0 import FlameFactory, AquaFactory, CreatureFactory
+
+
+def battle(c1: CreatureFactory, c2: CreatureFactory) -> None:
+    f = FlameFactory.create_base()
+    w = AquaFactory.create_base()
+    print(f"{f.describe()}\nvs.\n{w.describe()}")
+    print(f.attack())
+    print(w.attack())
+
+
+def check_create(factory: CreatureFactory) -> None:
+    f = factory.create_base()
+    print(f.describe())
+    print(f.attack())
+    f = factory.create_evolved()
+    print(f.describe())
+    print(f.attack())
+
+
+if __name__ == "__main__":
+    print("Testing factory")
+    check_create(FlameFactory())
+    print("\nTesting factory")
+    check_create(AquaFactory())
+    print("\nTesting battle")
+    battle(FlameFactory(), AquaFactory())
---
+++ ex0/__init__.py
@@ -0,0 +1,3 @@
+from .creature import FlameFactory as FlameFactory  # noqa: F401
+from .creature import AquaFactory as AquaFactory  # noqa: F401
+from .creature import CreatureFactory as CreatureFactory  # noqa: F401
